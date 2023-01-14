40355eafa3893a190d8d417d1e36f15538b73178
Kenneth Brook <kenneth.brook@hotmail.com>
2023-01-14 14:47:05 -0500

    logo added and linked

--- js/header.js
+++ js/header.js
@@ -1,10 +1,15 @@
 function header() {
-    console.log("Working");
     const pageHook = document.getElementById("mainContent");
-
     const wrap = document.createElement("div");
+    const logolink = document.createElement("a");
+    const logo = document.createElement("img");
 
     pageHook.appendChild(wrap);
+    wrap.appendChild(logolink);
+    logolink.href = "index.html";
+    logolink.appendChild(logo);
+    logo.src = "img/kclogo.png";
 
     wrap.classList.add("navWrap");
+    logo.classList.add("logo");
 }
